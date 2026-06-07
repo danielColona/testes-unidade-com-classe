11e836ff5ac3af7200120b0a62bd2cc593703426
danielColona <danielcolona@gmail.com>
2026-06-07 15:56:25 -0300

    Implementação 02 - Final

--- src/servicoDePagamento.js
+++ src/servicoDePagamento.js
@@ -1,19 +1,13 @@
 /*
-Crie uma classe que possua dois métodos: um para realizar pagamento e outro para consultar o último pagamento. Pagamentos serão armazenados como objetos Javascript dentro de uma lista de pagamentos. Cada pagamento terá as propriedades: Código de Barras, Empresa e Valor. Quando um pagamento for realizado e o valor for maior que 100.00, o pagamento também terá a propriedade 'categoria' preenchida pela função como 'cara', caso contrário, a propriedade 'categoria' será preenchida pela função como 'padrão'. O método de consultar trará apenas o último pagamento.
-  
-  ex. 
-  const servicoDePagamento = new ServicoDePagamento();
-  servicoDePagamento.pagar('0987-7656-3475', 'Samar', 156.87);
-  console.log(servicoDePagamento.consultarUltimoPagamento());
-  {
-     codigoBarras: '0987-7656-3475',
-     empresa: 'Samar',
-     valor: 56.87,
-     categoria: 'cara'
-  }
-  
-  A entregua deve ser realizada via Github e o repositório deve ter a classe a pasta src e os testes dos métodos dessa classe dentro da pasta test usando Mocha e Node Assert.
- */
+Crie uma classe que possua dois métodos: um para realizar pagamento e outro para consultar o último pagamento.
+Pagamentos serão armazenados como objetos Javascript dentro de uma lista de pagamentos. 
+Cada pagamento terá as propriedades: Código de Barras, Empresa e Valor. 
+Quando um pagamento for realizado e o valor for maior que 100.00, 
+o pagamento também terá a propriedade 'categoria' preenchida pela função como 'cara', 
+caso contrário, a propriedade 'categoria' será preenchida pela função como 'padrão'. 
+O método de consultar trará apenas o último pagamento.
+*/
+
 
 export default class ServicoDePagamento {
 
@@ -23,16 +17,28 @@ export default class ServicoDePagamento {
     this.#pagamentos = [];
   }
   
-  pagar (cod, empresa, valor, categoria) { 
+  pagar (cod, empresa, valor) { 
+  
     this.#pagamentos.push({
       codigoBarras: cod,
       empresa: empresa,
       valor: valor,
-      categoria: categoria
-    });
+      categoria: function () {
+        if (valor > 100) {
+          return 'cara';
+        } else {
+          return 'padrão';
+        }
+      }
+   });
   }
   
+  consultar() {
+    return this.#pagamentos;
+  }
+
   consultarUltimoPagamento() {
     return this.#pagamentos.at(-1);
   }
 }
+
--- test/servicoDePagamento.test.js
+++ test/servicoDePagamento.test.js
@@ -3,31 +3,38 @@ import assert from 'node:assert';
 
 describe('Classe de Serviço de Pagamento', () => {
 
-  it('Validar que o pagamento é adicionado na lista de #pagamento', () => {
-    const servicoDePagamento = new ServicoDePagamento();
-    servicoDePagamento.pagar('0987-7656-3475', 'Samar', 156.87);
-    console.log(servicoDePagamento.consultarUltimoPagamento());
-    {
-      codigoBarras: '0987-7656-3475',
-      empresa: 'Samar',
-      valor: 56.87,
-      categoria: 'cara'
-    }
-    
+  it('Validar o ultimo pagamento da lista e verificar se valor maior que R$100 retorna cara', () => {     
     //Arrange
     const servicoPag = new ServicoDePagamento();
-
     //Act
-    servicoPag.transferir('Daniel', 'Ariane', 50000);
-    const pagamentos = servicoPag.consultarUltimoPagamento();
-    const ultimoPagamento = pagamentos.at(-1);
+    servicoPag.pagar('0987-7656-3475', 'Microsoft', 50000);
+    const pagamento = servicoPag.consultarUltimoPagamento();
+    const ultimoPagamento = pagamento;
 
     //Assert
-    assert.equal(minhaTransferencia.remetente, 'Daniel');
-    assert.equal(minhaTransferencia.destinatario, 'Ariane');
-    assert.equal(minhaTransferencia.valorTransferencia, 50000);
+    assert.equal(ultimoPagamento.codigoBarras, '0987-7656-3475');
+    assert.equal(ultimoPagamento.empresa, 'Microsoft');
+    assert.equal(ultimoPagamento.valor, 50000);
+    const ultimoCat = ultimoPagamento.categoria();
+    assert.equal(ultimoCat, 'cara' ) ;
 
     });
     
+    it('Validar o ultimo pagamento da lista e verificar se valor menor que R$100 retorna padrão', () => {     
+    //Arrange
+    const servicoPag = new ServicoDePagamento();
+    //Act
+    servicoPag.pagar('0987-7656-0000', 'Microsoft', 80);
+    const pagamento = servicoPag.consultarUltimoPagamento();
+    const ultimoPagamento = pagamento;
+
+    //Assert
+    assert.equal(ultimoPagamento.codigoBarras, '0987-7656-0000');
+    assert.equal(ultimoPagamento.empresa, 'Microsoft');
+    assert.equal(ultimoPagamento.valor, 80);
+    const ultimoCat = ultimoPagamento.categoria();
+    assert.equal(ultimoCat, 'padrão' ) ;
+
+    });
 })
 
